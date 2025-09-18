86afdc058cf75cfb1cbd70ebfce679073b6fff70
Aikhan Zhashkeyev <spy.04@mail.ru>
2025-09-18 11:20:27 +0500

    FEAT: create ScheduleList and ScheduleItem components

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from 'react';
+import ScheduleList from '../components/app/ScheduleList/ScheduleList';
+
+export default function IndexPage(): ReactElement {
+  return (
+    <>
+      <ScheduleList />
+    </>
+  );
+}
